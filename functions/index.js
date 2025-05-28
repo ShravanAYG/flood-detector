fd96fdea7774c9ad24c9afc338fc0dc5c717f06f
Shravan A Y <shravanay205@gmail.com>
2025-05-28 21:47:59 +0530

    remove unnecessary

--- functions/index.js
+++
@@ -1,7 +0,0 @@
-const functions = require("firebase-functions");
-
-exports.echoMessage = functions.https.onRequest((req, res) => {
-  const message = req.body.message || "No message received.";
-  res.send(`Current message: ${message}`);
-});
-
